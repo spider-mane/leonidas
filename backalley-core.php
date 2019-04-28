26485ba0859237889f2231bacd36e71c2d6d98ff
Chris Williams <laserbeamvocalist@gmail.com>
2019-04-28 01:27:26 -0400

    corrected asset references

--- backalley-core.php
+++ backalley-core.php
@@ -6,23 +6,21 @@
 
 class Backalley_Base
 {
-    public static $path;
     public static $url;
+    public static $path;
+    public static $base;
     public static $admin_url;
     public static $admin_templates;
     public static $timber_locations;
 
     public static function load()
     {
+        Self::$base = plugin_basename(__FILE__);
         Self::$path = __DIR__;
         Self::$url = plugin_dir_url(__FILE__);
 
         Self::$admin_url = Self::$url . "public/admin";
         Self::$admin_templates = Self::$path . "/public/admin/templates";
-
-        // Self::$p = plugin_dir_path(__FILE__);
-        // Self::$plugin_url = plugin_dir_url(__FILE__);
-        // Self::$plugin = plugin_basename(__FILE__);
 
         Self::$timber_locations = [
             Self::$admin_templates,
