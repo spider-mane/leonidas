ff9ecb2cbd15d9ea174467bb740f714c09bc31b4
Chris Williams <spider.mane.web@gmail.com>
2021-03-15 19:37:40 -0400

    restructured phpunit testsuites

--- src/Library/Core/Helpers/functions.php
+++ src/Library/Core/Helpers/functions.php
@@ -2,7 +2,34 @@
 
 namespace Leonidas\Library\Core\Helpers;
 
-use function WebTheory\GuctilityBelt\sort_objects_array;
+/**
+ *
+ */
+function sort_objects_array(array $objects_array, array $order_array, string $order_key)
+{
+    usort($objects_array, function ($a, $b) use ($order_array, $order_key) {
+
+        foreach ([&$a, &$b] as &$obj) {
+            $obj = (int) $order_array[$obj->{$order_key}] >= 0 ? $order_array[$obj->{$order_key}] : 0;
+        }
+
+        if ($a === $b) {
+            return 0;
+        }
+
+        if ($a < $b && $a === 0) {
+            return 1;
+        }
+
+        if ($a > $b && $b === 0) {
+            return -1;
+        }
+
+        return $a > $b ? 1 : -1;
+    });
+
+    return $objects_array;
+}
 
 /**
  *
