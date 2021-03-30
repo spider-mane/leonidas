ca1179334ca3bfa2a805bc670df43d8102636dfc
Chris Williams <spider.mane.web@gmail.com>
2021-03-30 06:05:41 -0400

    symlink conditional

--- boot/development.php
+++ boot/development.php
@@ -3,8 +3,8 @@
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
-define('LEONIDAS_DEVELOPMENT', 1);
-
 ini_set('display_errors', '1');
 
-// (new Run())->prependHandler(new PrettyPageHandler())->register();
+(new Run())->prependHandler(new PrettyPageHandler())->register();
+
+define('LEONIDAS_DEVELOPMENT', 1);
