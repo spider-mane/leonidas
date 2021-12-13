77248e247aaca167736751168f47bcafa578cf78
Chris Williams <spider.mane.web@gmail.com>
2021-12-12 22:17:22 -0500

    SimpleCache Transient and TransientPool

---
+++ src/Library/Core/Cache/TransientPool.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Leonidas\Library\Core\Cache;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+class TransientPool implements CacheItemPoolInterface
+{
+    //
+}
---
+++ src/Library/Core/Cache/Transients.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Leonidas\Library\Core\Cache;
+
+use Psr\SimpleCache\CacheInterface;
+
+class Transients implements CacheInterface
+{
+    public function get($key, $default = null)
+    {
+        return get_transient($key) ?? $default;
+    }
+
+    public function set($key, $value, $ttl = null)
+    {
+        set_transient($key, $value, $ttl);
+    }
+
+    public function delete($key)
+    {
+        delete_transient($key);
+    }
+
+    public function clear()
+    {
+        //
+    }
+
+    public function getMultiple($keys, $default = null)
+    {
+        $values = [];
+
+        foreach ($keys as $key) {
+            $values[$key] = $this->get($key, $default);
+        }
+
+        return $values;
+    }
+
+    public function setMultiple($values, $ttl = null)
+    {
+        foreach ($values as $key => $value) {
+            $this->set($key, $value, $ttl);
+        }
+    }
+
+    public function deleteMultiple($keys)
+    {
+        foreach ($keys as $key) {
+            $this->delete($key);
+        }
+    }
+
+    public function has($key)
+    {
+        return false !== get_transient($key);
+    }
+}
