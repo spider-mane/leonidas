d18b792873fb39f2e0c8c8d4b5983065fe27f0be
Chris Williams <spider.mane.web@gmail.com>
2021-12-15 19:32:51 -0500

    hook traits using 'X' prefix for dynamic tag portions

---
+++ src/Traits/Hooks/TargetsAddMetaBoxesXPostTypeHook.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Leonidas\Traits\Hooks;
+
+use Closure;
+use WP_Post;
+
+trait TargetsAddMetaBoxesXPostTypeHook
+{
+    protected function targetAddMetaBoxesXPostTypeHook()
+    {
+        add_action(
+            "add_meta_boxes_{$this->getPostType()}",
+            $this->getAddMetaBoxesXPostTypeCallback(),
+            null,
+            PHP_INT_MAX
+        );
+
+        return $this;
+    }
+
+    protected function getAddMetaBoxesXPostTypeCallback(): Closure
+    {
+        return function (WP_Post $post) {
+            $this->doAddMetaBoxesXPostTypeAction($post);
+        };
+    }
+
+    abstract protected function getPostType(): string;
+
+    abstract protected function doAddMetaBoxesXPostTypeAction(WP_Post $post): void;
+}
---
+++ src/Traits/Hooks/TargetsXTaxonomyPreAddFormHook.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Leonidas\Traits\Hooks;
+
+use Closure;
+
+trait TargetsXTaxonomyPreAddFormHook
+{
+    protected function targetXTaxonomyPreAddFormHook()
+    {
+        add_action(
+            "{$this->getTaxonomy()}_pre_add_form",
+            $this->getXTaxonomyPreAddFormCallback(),
+            null,
+            PHP_INT_MAX
+        );
+
+        return $this;
+    }
+
+    protected function getXTaxonomyPreAddFormCallback(): Closure
+    {
+        return function (string $taxonomy) {
+            $this->doXTaxonomyPreAddFormAction($taxonomy);
+        };
+    }
+
+    abstract protected function getTaxonomy(): string;
+
+    abstract protected function doXTaxonomyPreAddFormAction(string $taxonomy): void;
+}
