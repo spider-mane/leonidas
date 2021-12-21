a260405e3e0b19df5beddde28dc4a48684f23201
Chris Williams <spider.mane.web@gmail.com>
2021-12-20 20:12:53 -0500

    composer autoload check in boot/init.php

--- boot/init.php
+++ boot/init.php
@@ -4,5 +4,9 @@ defined('ABSPATH') || exit;
 
 $root = dirname(__DIR__, 1);
 
+if (file_exists($composer = "$root/vendor/autoload.php")) {
+    require_once $composer;
+}
+
 require __DIR__ . '/functions.php';
 require __DIR__ . '/constants.php';
