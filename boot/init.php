37424485a2a8211dda73b57cc30cc628610af8e4
Chris Williams <spider.mane.web@gmail.com>
2021-12-20 19:52:36 -0500

    removed all coltrane references

--- boot/init.php
+++ boot/init.php
@@ -6,9 +6,3 @@ $root = dirname(__DIR__, 1);
 
 require __DIR__ . '/functions.php';
 require __DIR__ . '/constants.php';
-
-// require composer autoload if a site level composer.json has not been
-// specified by the user
-if (COLTRANE_COMPOSER_JSON === $root . '/composer.json') {
-    require COLTRANE_COMPOSER_DIR . '/autoload.php';
-}
