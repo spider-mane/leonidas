8622ad75cb8a4b4652c7aefd63a45727791dc4c9
Chris Williams <spider.mane.web@gmail.com>
2021-12-26 22:29:31 -0500

    removed Framework/Library/Theme

--- src/Framework/Theme/Theme.php
+++ src/Framework/Theme/Theme.php
@@ -31,4 +31,29 @@ class Theme
     {
         return static::TEMPLATE_TYPES;
     }
+
+    // public static function support(array $features)
+    // {
+    //     foreach ($features as $feature => $args) {
+
+    //         if (is_int($feature)) {
+    //             static::addSupport($args);
+    //         } else {
+    //             static::addSupport($feature, $args);
+    //         }
+    //     }
+    // }
+
+    // public static function addSupport(string $feature, $args = null)
+    // {
+    //     if (isset($args)) {
+    //         if (is_array($args) && array_is_list($args)) {
+    //             add_theme_support($feature, ...$args);
+    //         } else {
+    //             add_theme_support($feature, $args);
+    //         }
+    //     } else {
+    //         add_theme_support($feature);
+    //     }
+    // }
 }
