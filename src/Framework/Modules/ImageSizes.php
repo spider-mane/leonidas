72a1cfbb225f72a43109b521685840dcc0448f59
Chris Williams <spider.mane.web@gmail.com>
2021-12-30 04:44:40 -0500

    concrete ImageSizes module in base framework

---
+++ src/Framework/Modules/ImageSizes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Leonidas\Framework\Modules;
+
+use Leonidas\Contracts\Ui\Asset\ImageSizeCollectionInterface;
+use Leonidas\Library\Core\Asset\ImageSize;
+use Leonidas\Library\Core\Asset\ImageSizeCollection;
+
+class ImageSizes extends AbstractImageSizeProviderModule
+{
+    public const SIZES_CASCADE = [
+        'images.sizes', 'images', 'view.images.sizes', 'view.images',
+    ];
+
+    protected function definitions(): ImageSizeCollectionInterface
+    {
+        $sizes = $this->configCascade(static::SIZES_CASCADE);
+        $collection = new ImageSizeCollection();
+
+        foreach ($sizes as $name => $args) {
+            $collection->addSize(new ImageSize(
+                $name,
+                $args['label'],
+                $args['width'],
+                $args['height'],
+                $args['crop'] ?? false
+            ));
+        }
+
+        return $collection;
+    }
+}
