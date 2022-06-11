761eea6f483a53d7ef0d700b5b694180c9216ce5
Chris Williams <spider.mane.web@gmail.com>
2022-06-11 02:02:39 -0400

    extension loading logic moved to `ExtensionLoaderTrait` to eliminate duplication

--- src/Framework/Abstracts/ExtensionLauncherTrait.php
+++ src/Framework/Abstracts/ExtensionLauncherTrait.php
@@ -2,131 +2,16 @@
 
 namespace Leonidas\Framework\Abstracts;
 
-use Leonidas\Contracts\Extension\ExtensionBootProcessInterface;
-use Leonidas\Contracts\Extension\WpExtensionInterface;
-use Leonidas\Framework\ModuleInitializer;
-use Leonidas\Framework\WpExtension;
-use Panamax\Contracts\BootableProviderContainerInterface;
-use Panamax\Contracts\ContainerAdapterInterface;
-use Panamax\Contracts\ProviderContainerInterface;
-use Panamax\Contracts\ServiceContainerInterface;
-use Panamax\Contracts\ServiceCreatorInterface;
-use WebTheory\Config\Config;
-use WebTheory\Config\Interfaces\ConfigInterface;
-
 trait ExtensionLauncherTrait
 {
-    private string $path;
-
-    private string $url;
-
-    private ConfigInterface $config;
-
-    private ServiceContainerInterface $container;
-
-    private WpExtensionInterface $extension;
-
-    private static self $instance;
-
-    private function __construct(string $path, string $url)
-    {
-        $this->path = $path;
-        $this->url = $url;
-        $this->config = $this->defineConfig();
-        $this->container = $this->defineContainer();
-        $this->extension = $this->defineExtension();
-    }
-
-    protected function defineConfig(): ConfigInterface
-    {
-        return new Config($this->path . '/config');
-    }
-
-    private function defineContainer(): ServiceContainerInterface
-    {
-        return require $this->path . '/boot/container.php';
-    }
-
-    private function defineExtension(): WpExtensionInterface
-    {
-        $app = $this->config->get('app');
-
-        if ($this->container instanceof ContainerAdapterInterface) {
-            $container = $this->container->getAdaptedContainer();
-        }
-
-        return WpExtension::create([
-            'name' => $app['name'],
-            'version' => $app['version'],
-            'slug' => $app['slug'],
-            'prefix' => $app['prefix'],
-            'description' => $app['description'],
-            'path' => $this->path,
-            'url' => $this->url,
-            'type' => $app['type'],
-            'container' => $container ?? $this->container,
-            'dev' => $app['dev'],
-        ]);
+    use ExtensionLoaderTrait {
+        bootstrap as initiate;
     }
 
     private function bootstrap(): void
     {
-        $this
-            ->bindServicesToContainer()
-            ->initializeModules()
-            ->loadBootProcesses()
-            ->maybeBootProviders()
-            ->launch();
-    }
-
-    private function bindServicesToContainer(): self
-    {
-        $container = $this->container;
-
-        $container->share('root', $this->path);
-        $container->share('config', $this->config);
-
-        if ($container instanceof ProviderContainerInterface) {
-            $container->addServiceProviders(
-                $this->config->get('app.providers', [])
-            );
-        }
-
-        if ($container instanceof ServiceCreatorInterface) {
-            $container->createServices($this->config->get('app.services', []));
-        }
-
-        return $this;
-    }
-
-    private function initializeModules(): self
-    {
-        (new ModuleInitializer(
-            $this->extension,
-            $this->config->get('app.modules', [])
-        ))->init();
-
-        return $this;
-    }
-
-    private function loadBootProcesses(): self
-    {
-        foreach ($this->config->get('app.bootstrap', []) as $bootProcess) {
-            /** @var ExtensionBootProcessInterface $bootProcess */
-            $bootProcess = new $bootProcess();
-            $bootProcess->boot($this->extension, $this->container);
-        }
-
-        return $this;
-    }
-
-    private function maybeBootProviders(): self
-    {
-        if ($this->container instanceof BootableProviderContainerInterface) {
-            $this->container->bootServiceProviders();
-        }
-
-        return $this;
+        $this->initiate();
+        $this->launch();
     }
 
     private function launch(): void
--- src/Framework/ExtensionLoader.php
+++ src/Framework/ExtensionLoader.php
@@ -2,38 +2,19 @@
 
 namespace Leonidas\Framework;
 
-use Leonidas\Contracts\Extension\ExtensionBootProcessInterface;
 use Leonidas\Contracts\Extension\ExtensionLoaderInterface;
 use Leonidas\Contracts\Extension\WpExtensionInterface;
+use Leonidas\Framework\Abstracts\ExtensionLoaderTrait;
 use Leonidas\Framework\Exception\PluginInitiationException;
 use Leonidas\Framework\Exception\ThemeInitiationException;
-use Panamax\Contracts\BootableProviderContainerInterface;
-use Panamax\Contracts\ContainerAdapterInterface;
-use Panamax\Contracts\ProviderContainerInterface;
 use Panamax\Contracts\ServiceContainerInterface;
-use Panamax\Contracts\ServiceCreatorInterface;
-use WebTheory\Config\Config;
 use WebTheory\Config\Interfaces\ConfigInterface;
 
 class ExtensionLoader implements ExtensionLoaderInterface
 {
-    protected string $path;
-
-    protected string $url;
-
-    protected ServiceContainerInterface $container;
-
-    protected ConfigInterface $config;
-
-    protected WpExtensionInterface $extension;
-
-    public function __construct(string $path, string $url)
-    {
-        $this->path = $path;
-        $this->url = $url;
-        $this->container = $this->defineContainer();
-        $this->config = $this->defineConfig();
-        $this->extension = $this->defineExtension();
+    use ExtensionLoaderTrait {
+        __construct as public;
+        bootstrap as public;
     }
 
     public function getConfig(): ConfigInterface
@@ -49,47 +30,6 @@ class ExtensionLoader implements ExtensionLoaderInterface
     public function getExtension(): WpExtensionInterface
     {
         return $this->extension;
-    }
-
-    protected function defineConfig(): ConfigInterface
-    {
-        return new Config($this->path . '/config');
-    }
-
-    private function defineContainer(): ServiceContainerInterface
-    {
-        return require $this->path . '/boot/container.php';
-    }
-
-    public function defineExtension(): WpExtensionInterface
-    {
-        $data = $this->config->get('app');
-
-        if ($this->container instanceof ContainerAdapterInterface) {
-            $container = $this->container->getAdaptedContainer();
-        }
-
-        return WpExtension::create([
-            'name' => $data['name'],
-            'version' => $data['version'],
-            'slug' => $data['slug'],
-            'prefix' => $data['prefix'],
-            'description' => $data['description'],
-            'path' => $this->path,
-            'url' => $this->url,
-            'type' => $data['type'],
-            'container' => $container ?? $this->container,
-            'dev' => $data['dev'],
-        ]);
-    }
-
-    public function bootstrap(): void
-    {
-        $this
-            ->bindServicesToContainer()
-            ->initializeModules()
-            ->loadBootProcesses()
-            ->maybeBootProviders();
     }
 
     public function error(): void
@@ -111,55 +51,5 @@ class ExtensionLoader implements ExtensionLoaderInterface
                     $method
                 );
         }
-    }
-
-    protected function bindServicesToContainer(): ExtensionLoader
-    {
-        $container = $this->container;
-
-        $container->share('root', $this->path);
-        $container->share('config', $this->config);
-
-        if ($container instanceof ProviderContainerInterface) {
-            $container->addServiceProviders(
-                $this->config->get('app.providers', [])
-            );
-        }
-
-        if ($container instanceof ServiceCreatorInterface) {
-            $container->createServices($this->config->get('app.services', []));
-        }
-
-        return $this;
-    }
-
-    protected function initializeModules(): ExtensionLoader
-    {
-        (new ModuleInitializer(
-            $this->extension,
-            $this->config->get('app.modules', [])
-        ))->init();
-
-        return $this;
-    }
-
-    protected function loadBootProcesses(): ExtensionLoader
-    {
-        foreach ($this->config->get('app.bootstrap', []) as $bootProcess) {
-            /** @var ExtensionBootProcessInterface $bootProcess */
-            $bootProcess = new $bootProcess();
-            $bootProcess->boot($this->extension, $this->container);
-        }
-
-        return $this;
-    }
-
-    protected function maybeBootProviders(): ExtensionLoader
-    {
-        if ($this->container instanceof BootableProviderContainerInterface) {
-            $this->container->bootServiceProviders();
-        }
-
-        return $this;
     }
 }
