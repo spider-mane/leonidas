4bb8efda978b437d290b1b3916e566449a85552c
Chris Williams <spider.mane.web@gmail.com>
2024-04-11 20:06:27 -0400

    using explicit saveyour `RespectValidator` instead of `Validator` alias

--- src/Framework/Site/Form/ValidatesWithRespectMapTrait.php
+++ src/Framework/Site/Form/ValidatesWithRespectMapTrait.php
@@ -4,19 +4,19 @@ namespace Leonidas\Framework\Site\Form;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Respect\Validation\Validatable;
-use WebTheory\Saveyour\Validation\Validator;
+use WebTheory\Saveyour\Validation\RespectValidator;
 
 trait ValidatesWithRespectMapTrait
 {
     protected function validation(ServerRequestInterface $request): array
     {
         $validatables = $this->validatables($request);
-        $mapName = fn (Validatable &$v, string $rule) => $v->setName($rule);
+        $mapName = fn (Validatable $v, string $rule) => $v->setName($rule);
 
         foreach ($validatables as &$rules) {
             array_walk($rules, $mapName);
 
-            $rules = new Validator(...array_values($rules));
+            $rules = new RespectValidator(...array_values($rules));
         }
 
         return $validatables;
